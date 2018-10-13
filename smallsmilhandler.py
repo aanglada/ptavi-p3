d5ddaea93b8e6de6858af848d05ec38bf1fb30c0
Antonio <anto.nunez@hotmail.com>
2018-10-13 10:52:48 +0200

    No se si quereis que imprima algo o no, asi que e puesto un print por si acaso

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,41 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+
+class SmallSMILHandler(ContentHandler):
+
+        def __init__(self):
+
+            self.elementos = []
+            self.tags = ["root-layout", "region", "img", "audio", "textstream"]
+            self.atributos = {
+                "root-layout": ["width", "height", "background-color"],
+                "region": ["id", "top", "bottom", "left", "right"],
+                "img": ["src", "region", "begin", "dur"],
+                "audio": ["src", "begin", "dur"],
+                "textstream": ["src", "region"]
+                }
+
+        def startElement(self, name, attrs):
+
+            self.diccionario = {}
+            if name in self.tags:
+                self.diccionario['etiqueta'] = name
+                for atributo in self.atributos[name]:
+                    self.diccionario[atributo] = attrs.get(atributo, "")
+                self.elementos.append(self.diccionario)
+
+        def get_tags(self):
+
+            return self.elementos
+
+if __name__ == "__main__":
+
+    parser = make_parser()
+    myHandler = SmallSMILHandler()
+    parser.setContentHandler(myHandler)
+    parser.parse(open('karaoke.smil'))
+    print(myHandler.get_tags())
