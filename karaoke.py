7c0914607bce31bb5e79ddde8059bfc16e5afe41
Antonio <anto.nunez@hotmail.com>
2018-10-17 00:06:41 +0200

    Ya he conseguido pasarlo a objetos

--- karaoke.py
+++ karaoke.py
@@ -9,26 +9,36 @@ import json
 import sys
 
 
-def __str__(lista):
-    for etiquetas in lista:
-        elementos = []
-        for etiqueta in etiquetas:
-            if etiqueta != "element" and etiquetas[etiqueta] != "":
-                elementos += ("\t", etiqueta, '="', etiquetas[etiqueta], '"')
-        print(etiquetas["element"], "".join(elementos))
+class KaraokeLocal():
+
+    def __init__(self, fichero):
+        parser = make_parser()
+        sHandler = SmallSMILHandler()
+        parser.setContentHandler(sHandler)
+        parser.parse(open(fichero))
+        self.lista = sHandler.get_tags()
+
+    def __str__(self):
+        for etiquetas in self.lista:
+            elementos = []
+            for etiqueta in etiquetas:
+                if etiqueta != "element" and etiquetas[etiqueta] != "":
+                    elementos += ("\t", etiqueta, '="', etiquetas[etiqueta], '"')
+            print(etiquetas["element"], "".join(elementos))
 
 
-def to_json(lista, fichero):
-    fichjson = fichero.split('.')[0] + '.json'
-    json.dump(lista, open(fichjson, 'w'))
+    def to_json(self, fichero, jfichero):
+        if jfichero == "":
+            jfichero = fichero.split(".")[0] + ".json"
+        json.dump([self.lista], open(jfichero, "w"))
 
 
-def downloadLista(lista):
-    for etiquetas in lista:
-        for tag in etiquetas:
-            if tag == 'src' and etiquetas[tag].startswith('http://'):
-                urlretrieve(etiquetas[tag], etiquetas[tag].split('/')[-1])
-
+    def do_local(self):
+        for etiquetas in self.lista:
+            for tag in etiquetas:
+                if tag == 'src' and etiquetas[tag].startswith('http://'):
+                    urlretrieve(etiquetas[tag], etiquetas[tag].split('/')[-1])
+                    etiquetas[tag] = etiquetas[tag].split('/')[-1]
 
 if __name__ == "__main__":
 
@@ -36,11 +46,9 @@ if __name__ == "__main__":
         fichero = sys.argv[1]
     except:
         sys.exit("Usage: python3 karaoke.py file.smil")
-
-    parser = make_parser()
-    sHandler = SmallSMILHandler()
-    parser.setContentHandler(sHandler)
-    parser.parse(open(fichero))
-    __str__(sHandler.get_tags())
-    to_json(sHandler.get_tags(), fichero)
-    downloadLista(sHandler.get_tags())
+    karaoke = KaraokeLocal(fichero)
+    karaoke.__str__()
+    karaoke.to_json(fichero, '')
+    karaoke.do_local()
+    karaoke.to_json(fichero, "local.json")
+    karaoke.__str__()
